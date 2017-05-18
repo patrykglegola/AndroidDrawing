66fa47502ea822e7b9bbc16cce0c487a4d7d08bb
Patryk Glegoła <patryk.glegola@gmail.com>
2017-05-18 19:12:30 +0200

    Canvas.drawBitmap(...) doesn't work - it doesn't draw anything although bitmap is generated as we can see using debug mode.

--- app/src/main/java/pg/androiddrawing/DrawingSurface.java
+++ app/src/main/java/pg/androiddrawing/DrawingSurface.java
@@ -1,36 +1,49 @@
 package pg.androiddrawing;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-/**
- * Created by student on 05.05.17.
- */
 public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callback, Runnable {
     private SurfaceHolder holder;
     private Thread drawingThread;
-    private boolean threadIsWorking = false;
+    private boolean isThreadWorking = false;
     private Object lock = new Object();
+    private Bitmap bitmap = null;
+    private Canvas canvas = null;
+    private Paint paint;
+    private Path path = null;
 
     public DrawingSurface(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         holder = getHolder();
         holder.addCallback(this);
+
+        path = new Path();
+        paint = new Paint();
+        selectDefaultPaint();
+        paint.setStrokeWidth(2);
+        paint.setStyle(Paint.Style.STROKE);
+        selectDefaultPaint();
+
     }
 
     public void continueDrawing() {
         drawingThread = new Thread(this);
-        threadIsWorking = true;
+        isThreadWorking = true;
         drawingThread.start();
     }
 
     public void pauseDrawing() {
-        threadIsWorking = false;
+        isThreadWorking = false;
     }
 
 
@@ -41,7 +54,30 @@ public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callbac
         //sekcja krytyczna - modyfikacja rysunku na wyłączność
         synchronized (lock) {
             //modyfikacja rysunku...
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    //rysowanie...
+                    path.moveTo(event.getX(), event.getY());
+                    canvas.drawCircle(event.getX(),event.getY(), 5, paint);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    //rysowanie...
+                    path.lineTo(event.getX(), event.getY());
+                    break;
+                case MotionEvent.ACTION_UP:
+                    canvas.drawPath(path, paint);
+                    path.reset();
+                    canvas.drawCircle(event.getX(),event.getY(), 5, paint);
+
+                    break;
+                default:
+                    return false;
+
+            }
+            canvas.drawBitmap(bitmap, 0, 0, null);
         }
+        //canvas.drawBitmap(bitmap, 0, 0, null);
+        //canvas.drawPath(path, paint);
         return true;
     }
 
@@ -53,7 +89,7 @@ public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callbac
 
     @Override
     public void run() {
-        while(threadIsWorking) {
+        while(isThreadWorking) {
             Canvas canvas = null;
             try {
                 //sekcja krytyczna - żaden inny wątek nie może używać pojemnika
@@ -63,7 +99,7 @@ public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callbac
                         continue;
                     canvas = holder.lockCanvas(null);
                     synchronized (lock) {
-                        if (threadIsWorking) {
+                        if (isThreadWorking) {
                             //rysowanie na lokalnej kanwie
                         }
                     }
@@ -85,17 +121,52 @@ public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callbac
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
 
+        //tworzenie bitmapy i związanej z nią kanwy:
+        bitmap = Bitmap.createBitmap(getWidth(),getHeight(),Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        clear();
+        canvas.drawBitmap(bitmap, 0, 0, null);
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        //tworzenie bitmapy i związanej z nią kanwy:
+        bitmap = Bitmap.createBitmap(width,height,Bitmap.Config.ARGB_8888);
+        canvas = new Canvas(bitmap);
+        clear();
 
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
         //zatrzymanie rysowania
-        threadIsWorking = false;
+        isThreadWorking = false;
 
+    }
+
+    public void selectRedPaint() {
+        paint.setColor(Color.RED);
+    }
+
+    public void selectYellowPaint() {
+        paint.setColor(Color.YELLOW);
+    }
+
+    public void selectBluePaint() {
+        paint.setColor(Color.BLUE);
+    }
+
+    public void selectGreenPaint() {
+        paint.setColor(Color.GREEN);
+    }
+
+    public void clear() {
+        canvas.drawARGB(255,255,255,255);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        canvas.drawPath(path, paint);
+    }
+
+    public void selectDefaultPaint() {
+        selectRedPaint();
     }
 }
--- app/src/main/java/pg/androiddrawing/MainActivity.java
+++ app/src/main/java/pg/androiddrawing/MainActivity.java
@@ -2,12 +2,71 @@ package pg.androiddrawing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button redButton;
+    private Button yellowButton;
+    private Button blueButton;
+    private Button greenButton;
+    private Button clearButton;
+    private DrawingSurface drawingSurface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findAllButtons();
+        setupDrawingSurface();
+        setupButtonsListeners();
+    }
+
+    private void setupDrawingSurface() {
+        drawingSurface = (DrawingSurface) findViewById(R.id.activty_main_drawing_surface);
+    }
+
+    private void setupButtonsListeners() {
+        redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawingSurface.selectRedPaint();
+            }
+        });
+        yellowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawingSurface.selectYellowPaint();
+            }
+        });
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawingSurface.selectBluePaint();
+            }
+        });
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawingSurface.selectGreenPaint();
+            }
+        });
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawingSurface.clear();
+            }
+        });
+    }
+
+    private void findAllButtons() {
+        redButton = (Button) findViewById(R.id.activity_main_red_button);
+        yellowButton = (Button) findViewById(R.id.activity_main_yellow_button);
+        blueButton = (Button) findViewById(R.id.activity_main_blue_button);
+        greenButton = (Button) findViewById(R.id.activity_main_green_button);
+        clearButton = (Button) findViewById(R.id.activity_main_clear_button);
+
     }
 }
